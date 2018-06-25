9445c2f02c476af0b4a814b89260b2944c084374
shanghaikid <jiangruiyi@gmail.com>
2018-06-25 20:52:47 +0800

    format documents

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,27 +1,27 @@
-import {hyper, wire, bind, Component} from 'hyperhtml/esm';
-import './Button.scss';
+import { hyper, wire, bind, Component } from "hyperhtml/esm";
+import "./Button.scss";
 
 class Button extends HTMLElement {
-	static get observedAttributes() {
-		return ['label'];
-	}
-	constructor(...args) {
-		super(...args);
-		this.html = bind(this);
-	}
-	attributeChangedCallback() {
-		this.render();
-	}
-	connectedCallback() {
-		this.render();
-	}
-	render() {
-		return this.html`
-			<button>${this.getAttribute('label')}</button>
+    static get observedAttributes() {
+        return ["label"];
+    }
+    constructor(...args) {
+        super(...args);
+        this.html = bind(this);
+    }
+    attributeChangedCallback() {
+        this.render();
+    }
+    connectedCallback() {
+        this.render();
+    }
+    render() {
+        return this.html`
+			<button>${this.getAttribute("label")}</button>
 		`;
-	}
+    }
 }
 
-customElements.define('hyper-button', Button);
+customElements.define("hyper-button", Button);
 
-export {Button}
+export { Button };
--- src/components/Select.js
+++ src/components/Select.js
@@ -1,26 +1,26 @@
-import {hyper, wire, bind, Component} from 'hyperhtml/esm';
+import { hyper, wire, bind, Component } from "hyperhtml/esm";
 
 class Select extends HTMLElement {
-	static get observedAttributes() {
-		return ['label'];
-	}
-	constructor(...args) {
-		super(...args);
-		this.html = bind(this);
-	}
-	attributeChangedCallback() {
-		this.render();
-	}
-	connectedCallback() {
-		this.render();
-	}
-	render() {
-		return this.html`
+    static get observedAttributes() {
+        return ["label"];
+    }
+    constructor(...args) {
+        super(...args);
+        this.html = bind(this);
+    }
+    attributeChangedCallback() {
+        this.render();
+    }
+    connectedCallback() {
+        this.render();
+    }
+    render() {
+        return this.html`
 			<select></select>	
 		`;
-	}
+    }
 }
 
-customElements.define('hyper-select', Select);
+customElements.define("hyper-select", Select);
 
-export {Select}
+export { Select };
